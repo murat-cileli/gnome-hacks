6da7433f31c39c3de1836581426e84a8a62b36f3
Murat Çileli <murat.cileli@gmail.com>
2020-11-30 22:32:14 +0300

    Update gnome-hacks.sh

--- gnome-hacks.sh
+++ gnome-hacks.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
 
-hideClock=true
-darkTheme=( "kdenlive" "phpstorm" "krita" "gimp" )
+hideClock=false
+hidePanel=true
+darkTheme=( "kdenlive" "krita" "Blender" "Unity" "VirtualBox Machine" "Godot")
 
 if [[ $hideClock == true ]]; then
     dbus-send --session --type=method_call --dest=org.gnome.Shell /org/gnome/Shell org.gnome.Shell.Eval string:'Main.panel.statusArea.dateMenu.actor.hide();'
 fi
 
+if [[ $hidePanel == true ]]; then
+    dbus-send --session --type=method_call --dest=org.gnome.Shell /org/gnome/Shell org.gnome.Shell.Eval string:'Main.panel.set_height(1);'
+    dbus-send --session --type=method_call --dest=org.gnome.Shell /org/gnome/Shell org.gnome.Shell.Eval string:'Main.panel.statusArea.indicator.hide();'
+fi
+
+
 oldId=0
 
 while true
 do
-    sleep 3s;
+    sleep 5s;
 
-    id="$(nice -n10 xdotool getwindowfocus)"
+    id="$(nice -n19 xdotool getwindowfocus)"
 
     if [[ $oldId == $id ]]; then
         continue
     fi
 
-    class="$(nice -n10 xprop -id $(xdotool getwindowfocus) | grep WM_CLASS)"
+    class="$(nice -n19 xprop -id $id | grep WM_CLASS)"
 
     for i in "${darkTheme[@]}"
     do
         if [[ $class == *"$i"* ]]; then
-           nice -n10 xprop -id $id -f _GTK_THEME_VARIANT 8u -set _GTK_THEME_VARIANT "dark"
+           nice -n19 xprop -id $id -f _GTK_THEME_VARIANT 8u -set _GTK_THEME_VARIANT "dark"
         fi
     done
 
     oldId=$id
 done
-
